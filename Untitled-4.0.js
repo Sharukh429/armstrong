b8a8b295355976a8daf70c64fb85c0f6a4adbd4d
Dudekula Sharukh <dudekulasharuk4@gmail.com>
2025-07-10 13:25:23 +0530

    Add files via upload

---
+++ Untitled-4.0.js
@@ -0,0 +1,9 @@
+<script>
+ readCookie();
+
+ function readCookie() {
+    var value=document.cookie="tutorial=javascript";
+    if(value=="tutorial=javacript")
+        document.write("Cookie Value : javascript");
+ }
+ </script>
