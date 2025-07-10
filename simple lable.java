0427bef33ada14d17c4c890f9aba80b9a2363d51
Dudekula Sharukh <dudekulasharuk4@gmail.com>
2025-07-10 13:50:05 +0530

    Add files via upload

---
+++ simple lable.java
@@ -0,0 +1,22 @@
+import java.awt*;
+public class LableExample extends Frame
+{
+/Declare componentLable
+Lable|b|count;
+publicLable|exaqmple()
+{
+setLayout(newFlowLayout());
+//construct Lablel
+|b|count=newLable
+|b|count=newLable(":LableExample");
+//"super"Frame addsLable
+add(|b|count);
+setTitle("AWT Elements");
+setsize(200,200);
+setVisible(true);
+}
+public static void main(string[]args)
+{
+//allocating an instance
+lableExample app=newlableExample();
+}}s
